a2fc3e45d5b4116bd41daae12dd5f0ac9a36eadf
muyi2905 <obaremimuyiwa@gmail.com>
2024-01-15 12:08:25 +0000

    commit

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(requests):
-    return HttpResponse('blogapp')
+    return HttpResponse('<h1>Blogapp</h1>')
+
+def about(requests):
+    return HttpResponse('<h1>AboutBlog</h2>')
