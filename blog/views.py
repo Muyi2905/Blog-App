e7ef962479fe2d1234a6f6f05bd5b8b4a417c0b3
muyi2905 <obaremimuyiwa@gmail.com>
2024-01-15 16:10:46 +0000

    refactor

--- blog/views.py
+++ blog/views.py
@@ -4,20 +4,27 @@ from django.http import HttpResponse
 
 posts =[
     {
-        'author': 'Redice',
-        'book_title' : 'Solo Levelling',
-        'content': 'Hunter Sung-jun woo',
+        'author': 'Redice Studio',
+        'book_title' : '5G Internet',
+        'content': '5G internet, the fifth generation of mobile networks, delivers blazing-fast speeds, ultra-low latency, and enhanced connectivity. With data rates surpassing its predecessors, 5G empowers seamless streaming, rapid downloads, and supports the burgeoning Internet of Things (IoT), ushering in a new era of high-speed, responsive wireless communication.',
         'date_posted': '10 January,2024'
         
     },
     
         {
-        'author': 'George. R.R Martin',
-        'book_title' : 'Game of Thrones',
-        'content': 'House Lannister',
-        'date_posted': '16 March,1997'
+        'author': 'Kara Chambers',
+        'book_title' : 'Web Development',
+        'content': 'Web development is the art and science of creating websites and web applications. It involves designing, coding, and maintaining digital interfaces that users interact with. From front-end design for a visually appealing experience to back-end development for functionality, web development plays a crucial role in shaping the online world we navigate every day.',
+        'date_posted': '16 December,2023'
         
     },
+        
+        {
+        'author': 'Muyiwa Obaremi',
+        'book_title' : 'Misfit of Students',
+        'content': 'The misfit of students refers to individuals who may feel disconnected or out of sync with the mainstream academic environment. Whether due to unique learning styles, diverse interests, or unconventional approaches, addressing the needs of misfit students involves fostering inclusive educational spaces that cater to diverse talents and learning pathways, ensuring each student can thrive in their own way.',
+        'date_posted': '1 January,2024'
+        }
 ]
 
 
