07c4b998b114ccdd538237fd025d4c2a0a29f0fd
muyi2905 <obaremimuyiwa@gmail.com>
2024-01-15 12:44:57 +0000

    templates

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+dummy_posts =[
+    {
+        'author': 'Money Mase',
+        'book_title' : 'manchester',
+        'content': 'i love chelsea, i lied',
+        'date_posted': '10 January,2024'
+        
+    },
+    
+        {
+        'author': 'George. R.R Martin',
+        'book_title' : 'Game of Thrones',
+        'content': 'House Lannister',
+        'date_posted': '16 March,1997'
+        
+    },
+]
+
+
+
+
 def home(requests):
-    return HttpResponse('<h1>Blogapp</h1>')
+    return render(requests, 'blog\home.html')
 
 def about(requests):
-    return HttpResponse('<h1>AboutBlog</h2>')
+    return render(requests, 'about.html')
