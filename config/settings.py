65fa003bcb3482b9ee877e581f06f135774f7f27
muyi2905 <obaremimuyiwa@gmail.com>
2024-01-19 12:28:39 +0000

    commit

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'bootstrap4'
+     'crispy_bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -106,3 +106,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
